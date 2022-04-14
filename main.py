81b35e99710b2d20d2c8038cdf729146477eaccd
smasherdudemast <68716834+smasherdude@users.noreply.github.com>
2022-04-14 06:41:09 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,113 @@
+import os
+import discord
+from discord.ext import commands
+from fractions import Fraction
+import math
+from typing import Optional
+
+
+bot = commands.Bot(command_prefix="=")
+
+
+@bot.event
+async def on_command_error(ctx, error):
+    await ctx.send(f"An error occured: {str(error)}")
+    print(f"An error occured: {str(error)}")
+
+@bot.event
+async def on_ready():
+    print(f"Ready!\nUsername: {bot.user}")  #bot is online
+
+
+@bot.command()
+async def ping(ctx):  #ping
+    latency = round(bot.latency * 1000, 2)
+    await ctx.send(f"{latency} ms")
+
+
+@bot.command()
+async def test(ctx):  #random test that i should delete soon
+    await ctx.send("Test successful")
+
+
+@bot.command()
+async def server(ctx):  #server info and stuff
+    embed = discord.Embed(
+        title="Minecraft Server",
+        url=
+        "https://discord.com/channels/686062264192860211/864093314964652052/864093931843092480",
+        description="Join the Smasherdude Server",
+        color=discord.Color.blue())
+    embed.add_field(name="IP Address",
+                    value="smasherdude.aternos.me",
+                    inline=False)
+    embed.add_field(name="Version",
+                    value="1.17.1 on Java and the current version of Bedrock",
+                    inline=False)
+    await ctx.send(embed=embed)
+
+
+@bot.command()  #calculator
+async def calculate(ctx, *nums):
+    operation = " + ".join(nums)
+    await ctx.send(f'{operation} = {eval(operation)}')
+
+
+async def calculate(ctx, *nums):
+    operation = " - ".join(nums)
+    await ctx.send(f'{operation} = {eval(operation)}')
+
+
+async def calculate(ctx, *nums):
+    operation = " * ".join(nums)
+    await ctx.send(f'{operation} = {eval(operation)}')
+
+
+async def calculate(ctx, *nums):
+    operation = " / ".join(nums)
+    await ctx.send(f'{operation} = {eval(operation)}')
+
+
+@bot.command()
+async def percentof(ctx, a: int, b: int):
+    c = (a / 100) * b
+    await ctx.send(f'{a}% of {b} = {c}')
+
+
+@bot.command()
+async def ispercentof(ctx, a: int, b: int):
+    c = (a / b) * 100
+    await ctx.send(f'{a} is {c}% of {b}')
+
+
+@bot.command()
+async def hypotenuse(ctx, a: float, b: float):
+    c = (a * a) + (b * b)
+    await ctx.send(f'{math.sqrt(c)} is the hypotenuse of {a} and {b}')
+
+@bot.command()
+async def convert(ctx, args, a: float, b: Optional[int] = 5):
+    ftod = a/b
+    dtof = Fraction(a)
+    dtop = a * 100
+    ptod = a/100
+    ptof = Fraction(a/100)
+    ftop = (a/b) * 100
+    if args == 'ftod':
+      await ctx.send(f'{a}/{b} is converted to {ftod}')
+    elif args == 'dtof':
+      await ctx.send(f'{a} is {dtof}')
+    elif args == 'dtop':
+      await ctx.send(f'{a} is {dtop}%')
+    elif args == 'ptod':
+      await ctx.send(f'{a}% is {ptod}')
+    elif args == 'ptof':
+      await ctx.send(f'{a}% is {ptof}')
+    elif args == 'ftop':
+      await ctx.send(f'{a}/{b} is {ftop}%')
+    else: 
+      await ctx.send('an error has occured')
+
+
+token = os.environ['TOKEN']
+bot.run(token)
